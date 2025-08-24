e44e721460d9f3fa7dc40027abb564ede424f086
ryoma-abe <aryuu.appe@gmail.com>
2025-08-24 12:02:57 +0900

    不要なセクションコンポーネントのインポートをコメントアウトし、表示を一時的に無効化しました。これにより、コードの整理が進み、可読性が向上しました。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import HeroServer from "@/components/sections/hero/HeroServer";
 import AboutSection from "@/components/sections/about/AboutSection";
 import StrengthsSection from "@/components/sections/strengths/StrengthsSection";
-import WorksServer from "@/components/sections/WorksServer";
-import VoiceSection from "@/components/sections/voice/VoiceSection";
-import BlogServer from "@/components/sections/BlogServer";
-import FAQSection from "@/components/sections/faq/FAQSection";
+// import WorksServer from "@/components/sections/WorksServer";
+// import VoiceSection from "@/components/sections/voice/VoiceSection";
+// import BlogServer from "@/components/sections/BlogServer";
+// import FAQSection from "@/components/sections/faq/FAQSection";
 
 export default function Home() {
   return (
@@ -12,10 +12,10 @@ export default function Home() {
       <HeroServer />
       <AboutSection />
       <StrengthsSection />
-      <WorksServer />
+      {/* <WorksServer />
       <VoiceSection />
       <BlogServer />
-      <FAQSection />
+      <FAQSection /> */}
     </>
   );
 }
